0d913fdc0006de45a5ba9571065b2a61a43845f8
RuglesChaves <rugleschaves@gmail.com>
2022-05-02 15:26:19 -0400

    exemple html

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { engine } = require ('express-handlebars');
-
 const app = express();
-
+//config Handkebars engine
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+//fim config Handkebars energine
 
 //Rotas
-
 app.get('/cad', function(req,res){
     res.render('cad')
 })
+
+//fim de rotas
 
 app.listen(3000);
