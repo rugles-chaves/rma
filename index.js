838fba52fe244ef1fe19285efbb8d7d27604a994
RuglesChaves <rugleschaves@gmail.com>
2022-05-02 11:46:41 -0400

    Novo

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
+
+//Config tamplats energ
+
+app.engine('handlebars', handlebars({defaultlLayout: 'main'}))
+app.set('view engine', 'handlebrs')
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
