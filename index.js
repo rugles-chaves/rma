56fdde291178d845ec9c51623dc73bbe7d026822
RuglesChaves <rugleschaves@gmail.com>
2022-05-03 07:36:31 -0400

    path

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+//Modulos Instalado
 const express = require('express');
 const { engine } = require ('express-handlebars');
 const app = express();
-//config Handkebars engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-//fim config Handkebars energine
+const boryParser = require('bory-parser');
+const path = rewquire('path')
+
+//config
+
+    //config Handkebars engine
+        app.engine('handlebars', engine());
+        app.set('view engine', 'handlebars');
+    //fim config Handkebars energine
 
 //Rotas
 app.get('/cad', function(req,res){
     res.render('cad')
 })
-
 //fim de rotas
 
 app.listen(3000);
